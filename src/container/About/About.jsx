238c1f5028ca1ba820a8633e4ad890fc56023cd9
erichudson <defideveric@gmail.com>
2023-02-22 08:50:48 -0500

    First Commit

---
+++ src/container/About/About.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+import { images } from '../../constants';
+import './About.scss'
+
+const abouts =[ 
+  { title: 'Web Development', description: 'I am a good web developer', imgUrl: images.about01},
+  { title: 'Frontend Development', description: 'I am a good web developer', imgUrl: images.about02},
+  { title: 'Javascript | React', description: 'I am a good web developer', imgUrl: images.about03},
+  { title: 'UI Design', description: 'I am a good web developer', imgUrl: images.about04}
+]
+const About = () => {
+  return (
+    <>
+      <h2 className='head-text'>
+        I Know that <span>Good Apps</span><br />means <span>Good Business</span>
+      </h2>
+
+      <div className='app__profiles'>
+          {abouts.map((about, index) => (
+            <motion.div
+            whileInView={{opacity: 1}}
+            whileHover={{scale: 1.1}}
+            transiton={{duration:0.5, type: 'tween'}}
+            className='app__profile-item'
+            key={about.title + index}>
+            <img src={about.imgUrl} alt={about.title}/>
+            <h2 className='bold-text' style={{marginTop:20}}>{about.title}</h2>
+            <p className='p-text' style={{marginTop:10}}>{about.description}</p>
+            </motion.div>
+          ))}
+      </div>
+    </>
+  )
+}
+
+export default About;
