52537f49c61228bb3078d32baad3a77fabc55c57
erichudson <defideveric@gmail.com>
2023-02-23 08:06:20 -0500

    Work.jsx #2

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -24,9 +24,27 @@ const Work = () => {
     })
   })
 
+
+  //For works section buttons animation
   const handleWorkFilter = (item) => {
+    setActiveFilter(item);
+    setAnimateCard([{ y: 100, opacity: 0}]);
+
+    setTimeout(() => {
+      setAnimateCard([{ y: 0, opacity: 1 }]);
+
+      if(item === 'All') {
+        setFilterWork(works);
+      } else {
+        setFilterWork(works.filter((work) => work.tags.includes(item)))
+      }
+    }, 500);
 
   }
+
+
+
+
   return (
    <>
     <h2 className='head-text'>My Creative <span> Portfolio </span><br /><span> Section</span></h2>
